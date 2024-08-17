cd379da7a296649ae868afe30531c22bd6c7712f
Victor <u20201e188@upc.edu.pe>
2024-08-16 21:41:27 -0500

    Add: Project for movile course

--- app/src/main/java/com/u202021367/semana1_1_viernes/MainActivity.kt
+++ app/src/main/java/com/u202021367/semana1_1_viernes/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.u202021367.semana1_1_viernes.ui.theme.Semana1_1_viernesTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +24,29 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Semana1_1_viernesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            principal()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
+
 @Composable
-fun GreetingPreview() {
-    Semana1_1_viernesTheme {
-        Greeting("Android")
+fun principal() {
+
+    Column {
+        Text(text = "Hello, UPC!")
+        Text(text = "Desarrollo de aplicaciones móviles", Modifier
+            .background(Color.Yellow)
+            .padding(vertical = 30.dp)) // Codigo de color y padding en el texto
+        Text(text = "Clase 1")
+        Text(text = "Jetpack")
+
+        Row {
+            Text(text = "Android")
+            Text(text = "Kotlin")
+            Text(text = "Jetpack")
+        }
     }
+
 }
